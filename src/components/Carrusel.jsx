b22263b5353aa08b6efce99f87259b098ea668cb
Vina del Mar <vina@duoc.acad>
2025-10-06 16:32:49 -0300

    carrusel

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,5 +1,5 @@
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import CarouselFadeExample from 'src\components\Carrusel.jsx';
 
 function CarouselFadeExample() {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Row, Col, Card } from 'react-bootstrap'
+import CarouselFadeExample from 'src\components\Carrusel.jsx';
 
 
 export default function Home(){
@@ -12,6 +13,30 @@ return (
 </Col>
 </Row>
 <Row>
+<Carousel fade>
+<Carousel.Item>
+<ExampleCarouselImage text="First slide" />
+<Carousel.Caption>
+    <h3>First slide label</h3>
+    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+</Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+<ExampleCarouselImage text="Second slide" />
+<Carousel.Caption>
+    <h3>Second slide label</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+</Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+<ExampleCarouselImage text="Third slide" />
+<Carousel.Caption>
+    <h3>Third slide label</h3>
+    <p>
+    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+    </p>
+</Carousel.Caption>
+</Carousel.Item></Carousel>
 <Col md={4} className="mb-3">
 <Card>
 <Card.Body>
