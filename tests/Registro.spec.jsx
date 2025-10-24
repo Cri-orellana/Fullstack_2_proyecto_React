7ab1425b04a3bf05a9ed4d590a51998811d7d9c6
Vina del Mar <vina@duoc.acad>
2025-10-24 11:54:57 -0300

    se intenta agregar pruebas

--- tests/Registro.spec.jsx
+++ tests/Registro.spec.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import RegistroUsuario from './registroUsuario.jsx';
+import { describe, it, expect } from 'vitest'
 
-// `describe` agrupa un conjunto de pruebas relacionadas.
+import RegistroUsuario from './src/components/usuarios/registroUsuario.jsx';
+
 describe('Componente RegistroUsuario', () => {
 
-    // `it` define una prueba individual.
     it('debe renderizar el título de la pestaña de registro', () => {
-        // 1. Renderizamos el componente dentro de un Router porque usa `useNavigate`.
         render(
             <BrowserRouter>
                 <RegistroUsuario />
             </BrowserRouter>
         );
 
-        // 2. Buscamos un elemento que contenga el texto "Registro".
-        // `screen.getByText` busca un elemento visible con el texto exacto.
         const elementoTitulo = screen.getByText('Registro');
 
-        // 3. `expect` es la aserción. Verificamos que el elemento encontrado esté en el documento.
         expect(elementoTitulo).toBeInTheDocument();
     });
 
