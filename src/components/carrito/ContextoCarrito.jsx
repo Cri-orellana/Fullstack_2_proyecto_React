fb6accedc82ba380e8357abae4b6069344c58158
IgnacioOsorioG <ig.osoriog@duocuc.cl>
2025-10-24 12:04:29 -0300

    pequeños ajustes

--- src/components/carrito/ContextoCarrito.jsx
+++ src/components/carrito/ContextoCarrito.jsx
@@ -1,14 +1,11 @@
 import React, { createContext, useState, useContext } from 'react';
 
-// 1. Creamos el contexto
 const ContextoCarrito = createContext();
 
-// 2. Creamos un Hook personalizado para usar el contexto más fácilmente
 export const usarCarrito = () => {
     return useContext(ContextoCarrito);
 };
 
-// 3. Creamos el proveedor del contexto
 export const ProveedorCarrito = ({ children }) => {
     const [articulosCarrito, setArticulosCarrito] = useState([]);
 
@@ -40,11 +37,10 @@ export const ProveedorCarrito = ({ children }) => {
         setArticulosCarrito(articulosAnteriores =>
             articulosAnteriores.map(item => {
                 if (item.id === idProducto) {
-                    // Si la cantidad es mayor que 1, la reducimos
                     return { ...item, cantidad: item.cantidad > 1 ? item.cantidad - 1 : 1 };
                 }
                 return item;
-            }).filter(item => item.cantidad > 0) // Opcional: elimina si la cantidad llega a 0
+            }).filter(item => item.cantidad > 0) 
         );
     };
 
